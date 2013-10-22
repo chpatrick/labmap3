a7f276f6ddcf3e8a4320473462a406b06ed75620
Patrick Chilton <chpatrick@gmail.com>
2013-10-22 15:25:37 +0100

    Added configuration files.

--- daemon/Labmap.hs
+++ daemon/Labmap.hs
@@ -13,12 +13,28 @@ import Control.Concurrent
 import Control.Concurrent.Chan
 import Control.Monad
 import Data.Aeson
+import Data.Data
+import qualified Data.HashMap.Strict as H
 import qualified Data.ByteString.Lazy.Char8 as BSL8
 import Data.IORef
 import Data.Text (Text)
 import qualified Data.Map as M
 import System.Environment
+import System.Exit
 import System.Posix.Files
+
+data LabmapConf = LabmapConf
+  { sshOpts :: [ String ]
+  , machines :: Machines
+  , outputFile :: FilePath
+  } deriving (Read, Show)
+
+instance FromJSON LabmapConf where
+  parseJSON (Object conf)
+    = LabmapConf <$>
+        (words <$> (conf .: "sshOpts")) <*>
+	(H.toList <$> (conf .: "machines")) <*>
+	conf .: "outputFile"
 
 instance ToJSON User where
   toJSON u = object
@@ -39,13 +55,21 @@ main = do
     [ "getuser" ] -> getUser >>= print
     _ -> putStrLn "usage: labmap scan/getuser"
 
+loadConfig :: IO LabmapConf
+loadConfig = do
+  configText <- BSL8.readFile "labmap.conf"
+  case decode' configText of
+    Nothing -> putStrLn "Failed to parse configuration." >> exitFailure
+    Just c -> return c
+
 scanCommand :: IO ()
 scanCommand = do
+  config <- loadConfig
   users <- cache (hours 5) getAllUsers
   labState <- newIORef (M.empty)
   resultChan <- newChan
   self <- findSelf
-  scan opts machines [ self, "getuser " ] resultChan 8
+  scan (sshOpts config) (machines config) [ self, "getuser " ] resultChan 8
   forever $ do
     ( m, s ) <- readChan resultChan
     s' <- case s of
@@ -59,4 +83,4 @@ scanCommand = do
     ls <- readIORef labState
     let ls' = M.insert m s' ls
     writeIORef labState ls'
-    BSL8.putStrLn $ encode ls'
+    BSL8.writeFile (outputFile config) (encode ls')
--- daemon/Labmap/Scanner.hs
+++ daemon/Labmap/Scanner.hs
@@ -17,32 +17,9 @@ import Text.Regex.PCRE
 import Labmap.Common
 import Labmap.Util
 
-type SSHOpts = [ ( String, String ) ]
-
 type Hostname = String
 
 type Machines = [ ( Hostname, Int ) ]
-
--- TEMP HACKS
-
-opts :: SSHOpts
-opts =
-  [ ( "StrictHostKeyChecking", "no" )
-  , ( "UserKnownHostsFile", "/dev/null" )
-  , ( "PreferredAuthentications", "gssapi-with-mic" )
-  ]
-
-machines :: Machines
-machines
-  = [ ( "pixel", 37 )
-    , ( "texel", 38 )
-    , ( "matrix", 23 )
-    , ( "visual", 23 )
-    , ( "corona", 42 )
-    , ( "edge", 28 )
-    ]
-
-----
 
 enumerateMachines :: String -> Int -> [ String ]
 enumerateMachines group count
@@ -52,20 +29,19 @@ createWorkQueue :: Machines -> IO (LazyChan String)
 createWorkQueue machines
   = newMVar $ cycle $ concatMap (uncurry enumerateMachines) machines
 
-scanMachine :: SSHOpts -> [ String ] -> String -> IO (Maybe MachineState)
+scanMachine :: [ String ] -> [ String ] -> String -> IO (Maybe MachineState)
 scanMachine sshOpts cmd hostname = do
-  let sshArgs = concatMap (\( key, val ) -> [ "-o", key ++ "=" ++ val ] ) sshOpts
-  let args = (sshArgs ++ [ hostname ] ++ cmd)
+  let args = (sshOpts ++ [ hostname ] ++ cmd)
   ( exitCode, result, err ) <- readProcessWithExitCode "ssh" args ""
   return (guard (exitCode == ExitSuccess) >> readMaybe result)
 
-scanner :: SSHOpts -> [ String ] -> LazyChan Hostname -> Chan ( Text, Maybe MachineState ) -> IO ()
+scanner :: [ String ] -> [ String ] -> LazyChan Hostname -> Chan ( Text, Maybe MachineState ) -> IO ()
 scanner opts cmd work result = forever $ do
   hostname <- readLazyChan work
   state <- scanMachine opts cmd hostname
   writeChan result ( T.pack hostname, state )
 
-scan :: SSHOpts -> Machines -> [ String ] -> Chan ( Text, Maybe MachineState ) -> Int -> IO [ ThreadId ]
+scan :: [ String ] -> Machines -> [ String ] -> Chan ( Text, Maybe MachineState ) -> Int -> IO [ ThreadId ]
 scan opts machines cmd results threads = do
   wq <- createWorkQueue machines
   replicateM threads $ forkIO (scanner opts cmd wq results)
